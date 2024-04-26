402a79f60f3d216afea947bf2126d110b8222a94
agasthi23 <137294353+agasthi23@users.noreply.github.com>
2024-04-26 21:22:51 +0530

    0.07
    added teacher edit form

---
+++ Admin Panel - Login & Signup/Admin Panel - Login & Signup/UserControlTeacherEdit.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Data.SqlClient;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace Class_Pilot
+{
+    public partial class UserControlTeacherEdit : UserControl
+    {
+       
+        public UserControlTeacherEdit()
+        {
+            InitializeComponent();
+        }
+      
+
+        private void UserControlTeacherEdit_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+    }
+
+    
